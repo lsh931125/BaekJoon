bdea864fa3f4500ad0ce8c0b620301d6b148ba53
lsh931125 <77522110+lsh931125@users.noreply.github.com>
2021-05-03 22:54:25 +0900

    Delete str4.py

--- str4.py
+++
@@ -1,9 +0,0 @@
-# 문자열 반복
-import sys
-
-n = int(input())
-s = []
-for i in range(n):
-    s.append(list(map(str,sys.stdin.readline().split())))
-    for j in s[i][0]:
-        print(list(s[i][1])*int(j))
